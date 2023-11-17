f04eba4527c4beeecef651aac2d374c258809022
Dante Yupanqui <dantepapitorey.yr@gmail.com>
2023-11-17 10:10:06 -0400

    componente

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,4 @@
 import { useState } from 'react'
-<<<<<<< HEAD
-
-=======
 import Carrucel2 from '/home/carrucel2';
 import Cabecera from '/home/cabecera';
 import './App.css'
@@ -9,22 +6,7 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
 
-<<<<<<< HEAD
   
-=======
-  return (
-    <>
-     
-  
-      
-     <Carrucel1/>
-      <Carrucel2/>
-      <Cabecera/>
-      
-       
-    </>
-  )
->>>>>>> d937480f5c16878c26e77728ba1c29a30b7b7abf
 }
 
 export default App
