19874f1136badf132474f0385713f5ae43a71ef9
Thais11 <thaisflores110620@gmail.com>
2023-11-17 09:38:28 -0400

    commits dante

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,13 @@
 import { useState } from 'react'
-<<<<<<< HEAD
 
-=======
 import Home from '/home/home';
 import Cabecera from '/home/cabecera';
->>>>>>> d937480f5c16878c26e77728ba1c29a30b7b7abf
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
-<<<<<<< HEAD
   
-=======
   return (
     <>
       <Home/>
@@ -20,7 +15,6 @@ function App() {
        
     </>
   )
->>>>>>> d937480f5c16878c26e77728ba1c29a30b7b7abf
 }
 
 export default App
