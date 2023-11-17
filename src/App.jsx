c85301dc4e5746fa16f31a2b641b86a5363a2aa3
Thais11 <thaisflores110620@gmail.com>
2023-11-17 09:41:55 -0400

    cabezera mas cambios

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,33 @@ import { useState } from 'react'
 import Home from '/home/home';
 import Cabecera from '/home/cabecera';
 import './App.css'
+import FondoT from './components/FondoT'
+import {fondoT} from './assets/APIfondo'
 
 function App() {
   const [count, setCount] = useState(0)
-
   
-  return (
-    <>
-      <Home/>
-      <Cabecera/>
-       
-    </>
-  )
+      return(
+        <>
+        {
+          fondoT.map(fondoo=>{
+            return(
+              
+              <FondoT fondoT={fondoo}>
+              <Home/>
+              <Cabecera/>
+
+              
+              </FondoT>
+            )
+          })
+        } 
+          
+          
+        </>
+        
+        
+      )
 }
 
 export default App
