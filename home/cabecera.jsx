c94b70f0471e16f4ddb9c9c5be2815658469c414
Thais11 <thaisflores110620@gmail.com>
2023-11-17 11:27:19 -0400

    subiendo

--- home/cabecera.jsx
+++ home/cabecera.jsx
@@ -2,13 +2,6 @@ import '/styles/cabecera.css'
 function cabecera(props) 
 {
     const more=props.more
-    return(
-        <section className='cabera'>
-            <div className='orange-button'>
-                 <p>Learn More</p>
-            </div>
-        </section>
-    )
     
 }
 export default cabecera
